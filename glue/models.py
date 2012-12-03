ebac5d0b9188e5326954bba0d913f2738414f9d2
Daniele Guido <gui.daniele@gmail.com>
2012-12-03 23:16:31 +0000

    models added published flag

--- glue/models.py
+++ glue/models.py
@@ -10,54 +10,60 @@ class Geo( models.Model): # geo spot, with zoom
 	lon = models.FloatField() # map center LON
 	zoom = models.IntegerField() # start zoom
 	content = models.TextField( default="", blank=True, null=True ) # textual GEO description
-	
+
 class Pin( models.Model ):
-	slug = models.SlugField()
-	title = models.CharField( max_length=160, default="", blank=True, null=True )
-	abstract = models.TextField( default="", blank=True, null=True )
-	content = models.TextField( default="", blank=True, null=True )
-	language =  models.CharField( max_length=2, default='EN', choices=LANGUAGE_CHOICES ) # magic admin features: create a pin for the same language
-	
-	mimetype = models.CharField( max_length=255, default="", blank=True, null=True )
-	sort =  models.IntegerField( default=0 )
+    published='P'
+    draft='D'
 
-	date = models.DateField( blank=True, null=True ) # main date, manually added 
-	date_last_modified = models.DateField( auto_now=True ) # date last save()
-	
-	local = models.FileField( upload_to='pins/%Y-%m/',  blank=True, null=True ) # local stored file
-	permalink  = models.TextField( default="", blank=True, null=True ) # remote link
+    PIN_STATUS_CHOICES = ( (published,"published"),(draft,"draft") )
 
-	related = models.ManyToManyField("self", symmetrical=True, null=True, blank=True)
-	parent  = models.ForeignKey("self", null=True, blank=True, related_name="children" )
+    slug = models.SlugField()
+    title = models.CharField( max_length=160, default="", blank=True, null=True )
+    abstract = models.TextField( default="", blank=True, null=True )
+    content = models.TextField( default="", blank=True, null=True )
+    language =  models.CharField( max_length=2, default='EN', choices=LANGUAGE_CHOICES ) # magic admin features: create a pin for the same language
 
-	geos = models.ManyToManyField( Geo, blank=True, null=True ) # add geographic point
+    mimetype = models.CharField( max_length=255, default="", blank=True, null=True )
+    sort =  models.IntegerField( default=0 )
 
-	class Meta:
-		unique_together = ( "slug", "language" )
-		ordering = ('sort','id')
+    date = models.DateField( blank=True, null=True ) # main date, manually added
+    date_last_modified = models.DateField( auto_now=True ) # date last save()
 
-	def __unicode__(self):
-		return "%s (%s) a.k.a. %s" % (self.slug, self.language, self.title)
+    local = models.FileField( upload_to='pins/%Y-%m/',  blank=True, null=True ) # local stored file
+    permalink  = models.TextField( default="", blank=True, null=True ) # remote link
 
-	def json( self ):
-		return{
-			'id': self.id,
-			'slug':self.slug,
-			'title': self.title,
-			'abstract': self.abstract,
-			'content': self.content,
-			'language': self.language,
-			'mimetype': self.mimetype
-		}
+    related = models.ManyToManyField("self", symmetrical=True, null=True, blank=True)
+    parent  = models.ForeignKey("self", null=True, blank=True, related_name="children" )
+    status  = models.CharField( max_length=2, default="D",choices=PIN_STATUS_CHOICES)
+
+    geos = models.ManyToManyField( Geo, blank=True, null=True ) # add geographic point
+
+    class Meta:
+    	unique_together = ( "slug", "language" )
+    	ordering = ('sort','id')
+
+    def __unicode__(self):
+    	return "%s (%s) a.k.a. %s" % (self.slug, self.language, self.title)
+
+    def json( self ):
+    	return{
+    		'id': self.id,
+    		'slug':self.slug,
+    		'title': self.title,
+    		'abstract': self.abstract,
+    		'content': self.content,
+    		'language': self.language,
+    		'mimetype': self.mimetype
+    	}
 
 class PageAbstract( models.Model ):
 	slug = models.SlugField()
 	title = models.CharField( max_length=160, default="", blank=True, null=True )
 	abstract = models.TextField( default="", blank=True, null=True )
 	content = models.TextField( default="", blank=True, null=True )
-	
+
 	language =  models.CharField( max_length=2, default='EN', choices=LANGUAGE_CHOICES ) # magic admin features: create a pin for the same language
-	
+
 	class Meta:
 		unique_together = ( "slug", "language" )
 		abstract = True
@@ -78,4 +84,4 @@ class PageAbstract( models.Model ):
 class Page( PageAbstract ):
 	pins = models.ManyToManyField( Pin, null=True, blank=True, related_name="page")
 
-	
+
