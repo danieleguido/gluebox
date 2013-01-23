d9a20b57af709af78fad97eb25074f34baf660f9
Daniele Guido <gui.daniele@gmail.com>
2013-01-23 18:33:36 +0100

    Sitemap with links.

--- static/js/outside/sites.js
+++ static/js/outside/sites.js
@@ -2,4 +2,5 @@ oo.sites = {}
 
 oo.sites.init = function(){
 	$('li.site').popover({'trigger':'hover'});//.first().popover('show');
+	$('.link').popover({'trigger':'hover','placement':'bottom'});
 }
